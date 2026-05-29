c909fb6476f748674282372274f68916593bbb12
Park Hajun <dareum@kaist.ac.kr>
2026-05-29 02:02:29 -0700

    fix: more optimize f2_kernel

--- kernels.py
+++ kernels.py
@@ -217,20 +217,16 @@ def f2_kernel(
     tw_re = tl.load(tw_re_ptr + tl.arange(0, N // 2))
     tw_im = tl.load(tw_im_ptr + tl.arange(0, N // 2))
 
+    idx = tl.arange(0, N)
     # Butterfly stages
     for s in range(LOG2_N):
         # radix-2 butterfly
         half = 1 << s
-        idx = tl.arange(0, N)
 
         # find the partner index and twiddle index for each element
         partner_idx = idx ^ half
         partner_re = tl.gather(x_re, partner_idx, axis=0)
         partner_im = tl.gather(x_im, partner_idx, axis=0)
-
-        tw_idx = (idx % half) * (N >> (s + 1))
-        tw_re_s = tl.gather(tw_re, tw_idx, axis=0)
-        tw_im_s = tl.gather(tw_im, tw_idx, axis=0)
 
         # butterfly combine
         # W must multiply the lower element!!!
@@ -241,11 +237,19 @@ def f2_kernel(
         upper_re = tl.where(is_upper, x_re, partner_re)
         upper_im = tl.where(is_upper, x_im, partner_im)
 
-        tmp_re = lower_re * tw_re_s - lower_im * tw_im_s
-        tmp_im = lower_re * tw_im_s + lower_im * tw_re_s
+        if s == 0: # skip twiddle multiply in the first stage (to enhance performance)
+            x_re = tl.where(is_upper, upper_re + lower_re, upper_re - lower_re)
+            x_im = tl.where(is_upper, upper_im + lower_im, upper_im - lower_im)
+        else:
+            tw_idx = (idx % half) * (N >> (s + 1))
+            tw_re_s = tl.gather(tw_re, tw_idx, axis=0)
+            tw_im_s = tl.gather(tw_im, tw_idx, axis=0)
 
-        x_re = tl.where(is_upper, upper_re + tmp_re, upper_re - tmp_re)
-        x_im = tl.where(is_upper, upper_im + tmp_im, upper_im - tmp_im)
+            tmp_re = lower_re * tw_re_s - lower_im * tw_im_s
+            tmp_im = lower_re * tw_im_s + lower_im * tw_re_s
+
+            x_re = tl.where(is_upper, upper_re + tmp_re, upper_re - tmp_re)
+            x_im = tl.where(is_upper, upper_im + tmp_im, upper_im - tmp_im)
 
     idx = tl.arange(0, N)
 
